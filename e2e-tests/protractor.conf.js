68b70b831fa24c3e7acfb9c06fa4d15914b90ece
Philipp Katz <philipp@philippkatz.de>
2018-01-05 00:23:59 +0100

    Added protractor testing

---
+++ e2e-tests/protractor.conf.js
@@ -0,0 +1,25 @@
+//jshint strict: false
+exports.config = {
+
+  allScriptsTimeout: 11000,
+
+  specs: [
+    '*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: [ '--headless', '--disable-gpu', '--window-size=800,600' ]
+    }
+  },
+
+  baseUrl: 'http://localhost:8000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+
+};
---
+++ e2e-tests/scenarios.js
@@ -0,0 +1,20 @@
+'use strict';
+/* global element, by, browser, describe, beforeEach, it, expect */
+
+/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
+describe('angular-trix', function() {
+
+  beforeEach(function() {
+    browser.get('/e2e-tests');
+  });
+
+  // https://github.com/sachinchoolur/angular-trix/pull/2
+  // http://plnkr.co/edit/XQsKXpsH7LZ4m0XBkaR2
+  it('does not update model when there is no change by user', function() {
+    element(by.id('setSampleText')).click();
+    expect(element(by.id('pristine')).getText()).toEqual('true');
+    expect(element(by.id('modelText')).getText()).toEqual('the quick brown fox');
+  });
+
+});
